50083a1be337e05734b3ade70ad67c63d4793a50
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2022-02-15 02:38:43 +0500

    add

--- PHPBasics/typeConversion.php
+++ PHPBasics/typeConversion.php
@@ -8,6 +8,15 @@
 <body>
     <h1>type conversion</h1>
     <?php
+    //implicit conversion which means it is automatic
+    $result = 10  + "20.5" + true;
+    // we should be aware of types
+    //scaller types which can't be further classified like boolean, int, string, float
+    //composite data type that can store multiple values like arrays, Objects defined can have multiple values
+    // resource type this is linked with OS level data type, used for file handling and database, network programming (socket) they all are resource
+    // resource data type can't be converted they are neither explicit nor implicit
+
+    //scaller conversion
 
     ?>
 </body>
